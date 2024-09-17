7fd6dd323c61505fc6b32c8c66320ae2f05c8274
Divine <officialkb17@gmail.com>
2024-09-17 10:20:56 +0100

    Add "AddConsumer" contract

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -71,4 +71,18 @@ contract RaffleTest is Test {
         // Assert
         raffle.enterRaffle{value: entranceFee}();
     }
+
+    function testDontAllowPlayersToEnterWhileRaffleIsCalculating() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
+
+        // Act / Assert
+        vm.expectRevert(Raffle.Raffle__RaffleNotOpen.selector);
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+    }
 }
